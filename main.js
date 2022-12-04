3159c96adcb082fa9a821479fdebe17decd16896
filipjt88 <filip88ks@gmail.com>
2022-12-04 19:19:50 +0100

    save

---
+++ main.js
@@ -0,0 +1,13 @@
+let modalBtn = document.querySelector(".modal-btn");
+let modal      = document.querySelector(".modal-overlay");
+let closeBtn  = document.querySelector(".close-btn");
+
+
+modalBtn.addEventListener("click", () => {
+    modal.classList.add('open-modal');
+});
+
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove('open-modal');
+});
